54007840fbb8d8aa39a21e2d1daac7e0a5265d83
Wander06 <wandersonpsilva7@gmail.com>
2023-06-07 16:02:13 -0300

    primeiro-commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// `` crase
+
+function pedra(){
+    var escolhaPlayer = document.getElementById("player");
+
+    escolhaPlayer.innerHTML = `Voce escolheu <b>PEDRA</b>`
+}
+
+function papel(){
+    var escolhaPlayer = document.getElementById("player");
+
+    escolhaPlayer.innerHTML = `Voce escolheu <b>PAPEL</b>`
+}
+
+function tesoura(){
+    var escolhaPlayer = document.getElementById("player");
+
+    escolhaPlayer.innerHTML = `Voce escolheu <b>TESOURA</b>`
+}
